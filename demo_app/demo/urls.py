c3d77fec41ef9d485ab4f7d4bc50c53aff84ebe0
ybx <87212426@qq.com>
2015-03-09 01:20:22 +0800

    add fiel

---
+++ demo_app/demo/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+import xadmin
+
+from xadmin.plugins import xversion
+xversion.register_models()
+
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^markdown/', include("django_markdown.urls")),
+    url(r'^blog/', include('blog.urls')),
+    url(r'^', include(xadmin.site.urls)),
+)
